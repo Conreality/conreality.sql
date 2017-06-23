be9c5e0d30e0cd3e462d5f557b7d695bdb2fd458
Arto Bendiken <arto@conreality.org>
2017-06-23 02:38:19 +0200

    Restructured the source files.

--- src/schema.sh
+++ src/schema.sh
@@ -1,29 +1,50 @@
 #!/bin/sh
+function execute { echo "-- $1"; echo; cat "$1"; echo; }
+
+# Schema
 echo "DROP SCHEMA IF EXISTS conreality CASCADE;"; echo
 echo "CREATE SCHEMA conreality;"; echo
-cat _extensions.sql;   echo
 
-# dependency order:
-cat binary.table.sql;  echo
-cat theater.table.sql; echo
-cat group.table.sql;   echo
-cat object.table.sql;  echo
-cat object.procs.sql;  echo
+# Extensions
+execute _extensions.sql
 
-# alphabetical order:
-cat asset.table.sql;   echo
-cat camera.table.sql;  echo
-cat camera_frame.table.sql; echo
-cat event.table.sql;   echo
-cat event.procs.sql;   echo
-cat message.table.sql; echo
-cat message.procs.sql; echo
-cat player.table.sql;  echo
-cat player.procs.sql;  echo
-cat point.procs.sql;   echo
-cat session.procs.sql; echo
-cat target.table.sql;  echo
+# Table references
+execute binary.table.sql
+execute theater.table.sql
+execute group.table.sql
+execute object.table.sql
 
-# dependency order:
-cat message.views.sql; echo
-cat object.views.sql;  echo
+# Tables
+execute asset.table.sql
+execute camera.table.sql
+execute camera_frame.table.sql
+execute event.table.sql
+execute message.table.sql
+execute player.table.sql
+execute target.table.sql
+
+# Views
+execute message_with_avatar.view.sql
+execute object_asset.view.sql
+execute object_camera.view.sql
+execute object_player.view.sql
+execute object_target.view.sql
+
+# Functions
+execute event_send.function.sql
+execute message_send.function.sql
+execute object_invert_mass.function.sql
+execute object_is_accelerating.function.sql
+execute object_is_active.function.sql
+execute object_is_immovable.function.sql
+execute object_is_inactive.function.sql
+execute object_is_located.function.sql
+execute object_is_moving.function.sql
+execute object_is_rotating.function.sql
+execute player_deregister.function.sql
+execute player_register.function.sql
+execute point_2d.function.sql
+execute point_3d.function.sql
+execute point_gps.function.sql
+execute session_start.function.sql
+execute session_terminate.function.sql
