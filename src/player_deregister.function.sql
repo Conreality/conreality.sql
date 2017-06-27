f639c9f7cddfa1976b405bfa3c1cb49508d34d3f
Arto Bendiken <arto@conreality.org>
2017-06-27 11:48:29 +0200

    Fixed player registration and deregistration.
    Users needed schema access privileges. Also, the current_schema()
    function was returning "public" instead of "conreality".

--- src/player_deregister.function.sql
+++ src/player_deregister.function.sql
@@ -4,11 +4,15 @@ CREATE FUNCTION conreality.player_deregister(player_uuid uuid) RETURNS void AS $
 BEGIN
   DELETE FROM conreality.player p WHERE p.uuid = player_uuid;
   DELETE FROM conreality.object o WHERE o.uuid = player_uuid;
+  -- Permissions for the "pllua" schema:
   EXECUTE format('REVOKE SELECT ON TABLE %I.%I FROM %I', 'pllua', 'init', player_uuid);
   EXECUTE format('REVOKE USAGE ON SCHEMA %I FROM %I', 'pllua', player_uuid);
-  EXECUTE format('REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA %I FROM %I', current_schema, player_uuid);
-  EXECUTE format('REVOKE USAGE, SELECT ON ALL SEQUENCES IN SCHEMA %I FROM %I', current_schema, player_uuid);
-  EXECUTE format('REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %I FROM %I', current_schema, player_uuid);
+  -- Permissions for the "conreality" schema:
+  EXECUTE format('REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA %I FROM %I', 'conreality', player_uuid);
+  EXECUTE format('REVOKE USAGE, SELECT ON ALL SEQUENCES IN SCHEMA %I FROM %I', 'conreality', player_uuid);
+  EXECUTE format('REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %I FROM %I', 'conreality', player_uuid);
+  EXECUTE format('REVOKE USAGE ON SCHEMA %I FROM %I', 'conreality', player_uuid);
+  -- Permissions for the database itself:
   EXECUTE format('REVOKE CONNECT ON DATABASE %I FROM %I', current_database(), player_uuid);
   EXECUTE format('DROP USER %I', player_uuid);
 END;
--- src/player_register.function.sql
+++ src/player_register.function.sql
@@ -16,13 +16,18 @@ $$ LANGUAGE plpgsql VOLATILE PARALLEL UNSAFE;
 CREATE FUNCTION conreality.player_register(player_uuid uuid,
                                            player_nick text) RETURNS void AS $$
 BEGIN
+  -- Permissions for the database itself:
   EXECUTE format('CREATE USER %I WITH PASSWORD NULL', player_uuid);
   EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', current_database(), player_uuid);
-  EXECUTE format('GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %I TO %I', current_schema, player_uuid);
-  EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA %I TO %I', current_schema, player_uuid);
-  EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA %I TO %I', current_schema, player_uuid);
+  -- Permissions for the "conreality" schema:
+  EXECUTE format('GRANT USAGE ON SCHEMA %I TO %I', 'conreality', player_uuid);
+  EXECUTE format('GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA %I TO %I', 'conreality', player_uuid);
+  EXECUTE format('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA %I TO %I', 'conreality', player_uuid);
+  EXECUTE format('GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA %I TO %I', 'conreality', player_uuid);
+  -- Permissions for the "pllua" schema:
   EXECUTE format('GRANT USAGE ON SCHEMA %I TO %I', 'pllua', player_uuid);
   EXECUTE format('GRANT SELECT ON TABLE %I.%I TO %I', 'pllua', 'init', player_uuid);
+
   INSERT INTO conreality.object (uuid, type, label) VALUES (player_uuid, 'player', player_nick);
   INSERT INTO conreality.player (uuid, nick) VALUES (player_uuid, player_nick);
 END;
