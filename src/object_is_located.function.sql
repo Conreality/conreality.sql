349734e2b40ca5a16a483228add69671fcdc9cdd
Arto Bendiken <arto@conreality.org>
2017-07-01 03:06:24 +0200

    Implemented object_is_located().

--- src/object_is_located.function.sql
+++ src/object_is_located.function.sql
@@ -1,6 +1,6 @@
-DROP FUNCTION IF EXISTS conreality.object_is_located(text) RESTRICT;
+DROP FUNCTION IF EXISTS conreality.object_is_located(uuid) RESTRICT;
 
 -- Determines whether this object has a nonzero position.
-CREATE FUNCTION conreality.object_is_located(object_uuid text) RETURNS boolean AS $$
-  return nil -- unknown
-$$ LANGUAGE pllua STABLE PARALLEL SAFE;
+CREATE OR REPLACE FUNCTION conreality.object_is_located(object_uuid uuid) RETURNS boolean AS $$
+  SELECT COUNT(m.uuid) > 0 FROM conreality.object_motion m WHERE m.uuid = object_uuid;
+$$ LANGUAGE sql STABLE PARALLEL SAFE;
