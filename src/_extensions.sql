4bd23c382f78e48497a4147335d8f555477547d1
Arto Bendiken <arto@conreality.org>
2017-08-05 02:44:31 +0200

    Enabled the pgcrypto extension.

--- src/_extensions.sql
+++ src/_extensions.sql
@@ -2,6 +2,10 @@
 -- See: https://www.postgresql.org/docs/current/static/uuid-ossp.html
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Enable pgcrypto
+-- See: https://www.postgresql.org/docs/current/static/pgcrypto.html
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 -- Enable PL/Lua
 -- See: http://pllua.projects.pgfoundry.org/
 CREATE EXTENSION IF NOT EXISTS pllua;
