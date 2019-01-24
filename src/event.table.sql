aae819557df3d42d36b15330fd5f54ab4b67605a
Arto Bendiken <arto@conreality.org>
2019-01-24 16:43:14 +0200

    Ensured event.subject is nullable (for system events).

--- src/event.table.sql
+++ src/event.table.sql
@@ -7,8 +7,8 @@ CREATE TABLE conreality.event (
   timestamp timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
   -- The event's predicate (an action or verb).
   predicate varchar(63) NOT NULL CHECK (predicate <> ''),
-  -- The subject of the predicate.
-  subject   bigint NOT NULL REFERENCES conreality.entity ON DELETE CASCADE,
-  -- The object of the predicate.
+  -- The subject of the predicate (a player or asset; NULL indicates the system).
+  subject   bigint NULL REFERENCES conreality.entity ON DELETE CASCADE,
+  -- The object of the predicate (a player or asset; NULL indicates the system).
   object    bigint NULL REFERENCES conreality.entity ON DELETE SET NULL
 );
